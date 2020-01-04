d37432a559e4768a024f09962c4b240c7c05afc2
Sreetam Das <sreetamdas@gmail.com>
2020-01-04 19:09:03 +0530

    refactor: Components => components

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
+import { Canvas } from "./canvas";
 
-const App: React.FC = props => {
-    return (
-        <React.Fragment>
-            <p>
-                Edit{" "}
-                <code style={{ backgroundColor: "lightgrey", padding: "5px" }}>
-                    App.tsx
-                </code>
-            </p>
-        </React.Fragment>
-    );
+const App: React.FC = () => {
+	return (
+		<React.Fragment>
+			<Canvas />
+		</React.Fragment>
+	);
 };
 
 export default App;
